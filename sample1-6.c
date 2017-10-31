3265a91643563695b7cdcbebca729ceb9f8e7f2f
tarotez <tez@sings.jp>
2017-10-31 14:32:16 +0900

    return value set, and the name of a function revised

--- sample1-6.c
+++ sample1-6.c
@@ -4,16 +4,17 @@ int main(){
   int in, fact;
   printf("Type in a number: ");
   scanf("%d", &in);
-  if(in == 1){ printf("1 is not prime.\n"); return;}
+  if(in == 1){ printf("1 is not prime.\n"); return 0;}
   fact = isNotPrime(in);
   if(fact > 0){
     printf("%d is not prime. It can be divided by %d.\n", in, fact);
   }else{
     printf("%d is prime.\n", in);
   }
+  return 0;
 }
 
-int isNotPrime(int a){
+int getSmallestFactor(int a){
   int i = 2;
   while(i*i <= a){
     if(a % i == 0){
